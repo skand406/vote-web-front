fbdc9a214f744d97fd6a807e220bf426b31f0171
skand406 <chlxx0040639@gmail.com>
2023-07-02 15:42:46 +0900

    fix : 포트 변경 80

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     proxy: {
       '/api': {
         // '/api' 로 들어오면 포트 8080(스프링 서버)로 보낸다
-        target: 'http://localhost:8080',
+        target: 'http://localhost:80',
         changeOrigin: true // cross origin 허용
       }
     }
