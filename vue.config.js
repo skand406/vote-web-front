0dc6603f3d745efe6a4feaaab3e4288a348f9f0d
skand406 <chlxx0040639@gmail.com>
2023-07-03 13:57:24 +0900

    feat : 개발 환경 세팅

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        // '/api' 로 들어오면 포트 8080(스프링 서버)로 보낸다
-        target: 'http://localhost:80',
+        // '/api' 로 들어오면 포트 8888(스프링 서버)로 보낸다
+        target: 'http://localhost:8888',
         changeOrigin: true // cross origin 허용
       }
     }
