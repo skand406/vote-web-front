93ef4e6494b9f34c83528ca0ab64d118a74fcbe1
chohyeon <chohyeonK3@gmail.com>
2023-07-06 22:19:27 +0900

    디렉터리 추가&경로 변경 - manager, user, admin 별도 분리

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router';
-import HomeView from '../components/views/Home.vue'
-import LoginView from '../components/views/Login.vue'
-import SignupView from '../components/views/Signup.vue'
+import HomeView from '../components/views/manager/Home.vue'
+import LoginView from '../components/views/manager/Login.vue'
+import SignupView from '../components/views/manager/Signup.vue'
 
 
 const routes = [
